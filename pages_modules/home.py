fa34afd05173f12f79e3051f70f1d375a717d541
Pham Trong Quy <104240232@student.vgu.edu.vn>
2025-12-02 21:30:36 +0700

    [UPDATE]: Update Stun Google

--- pages_modules/home.py
+++ pages_modules/home.py
@@ -25,7 +25,13 @@ from sample_utils.download import download_file
 # ==========================================
 # Cấu hình STUN server của Google (Miễn phí & Ổn định)
 RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    {"iceServers": [
+        {"urls": ["stun:stun.l.google.com:19302"]},
+        {"urls": ["stun:stun1.l.google.com:19302"]},
+        {"urls": ["stun:stun2.l.google.com:19302"]},
+        {"urls": ["stun:stun3.l.google.com:19302"]},
+        {"urls": ["stun:stun4.l.google.com:19302"]},
+    ]}
 )
 
 HERE = Path(__file__).parent
